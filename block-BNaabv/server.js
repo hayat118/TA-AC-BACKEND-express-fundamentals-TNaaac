54ca4007fe998f780e5ad26a9e6a8e29a98c1e81
ravi11o <ravi.suraj110@gmail.com>
2022-10-14 19:41:52 +0530

    express assignment 1

--- block-BNaabv/server.js
+++ block-BNaabv/server.js
@@ -9,7 +9,10 @@ app.use(logger());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
-
+app.use((req,res,next)=>{
+  res.cookie("username","xyz");
+  next();
+})
 
 // routes
 app.get('/',(req,res)=>{
@@ -19,14 +22,22 @@ app.get('/about',(req,res)=>{
   res.send('My name is qwerty')
 })
 app.post('/form',(req,res)=>{
-  console.log(req.body)
+  res.json(req.body)
 })
 app.post('/json',(req,res)=>{
-  console.log(req.body)
+  res.json(req.body)
 })
 app.get('/users/:username',(req,res)=>{
   var username=req.params.username;
-  res.send(username)
+  res.send(`<h2>${username}<h2>`)
+})
+// 404 error
+app.use((req,res,next)=>{
+  res.send('Page not found')
+})
+// custom error
+app.use((err,req,res,next)=>{
+  res.send(err)
 })
 
 
